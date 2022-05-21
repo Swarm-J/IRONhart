825324d36404528b57e471e06efc8cff2421844a
Swarm-J <julianschinkel@gmail.com>
2022-05-21 19:46:50 +0200

    added error pages

--- app.py
+++ app.py
@@ -44,6 +44,14 @@ class InquiryForm(FlaskForm):
     message = TextAreaField("" ,validators=[DataRequired(message="A message is required!"), Length(max=400)])
     submit = SubmitField("Submit")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template("index.html")
